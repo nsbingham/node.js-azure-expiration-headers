bcefe36eeeb759b9e1d43ec5592eb1261cb47511
Nathan Bingham <nsbingham@gmail.com>
2013-10-03 10:34:02 -0400

    Updating the default service version.

--- blob-headers.js
+++ blob-headers.js
@@ -4,11 +4,29 @@
    "use strict";
  
     var azure = require('azure');
-    var blobService = azure.createBlobService("modernievirt","PHahBCdmHiS34IeXOF9RWCGz8Urk9SZmSGe2bRRCFbYfj74R7CG5GH+4FYEJll/xiyr4UOjWZcGsYwUMSW8INA==");
+    var blobService = azure.createBlobService("winstoreprod","o8ZpcpuqZogcuAW7pKf7CLF9NcWs0Yus4m9FMLWQBPnq6eZC9l4AwCimP2/J5TlnC8/Rs2NkwhbXrgFv0xhS1Q==");
     var containerName = "vhd";
     var cacheControlLength = "604800"; // 7 days in seconds
 
-    function setHeaders(blobName){
+    blobService.getServiceProperties(function(error, properties) {
+
+        console.log('Get service callback.', properties);
+
+        if(error) throw error;
+
+        properties['DefaultServiceVersion'] = '2013-03-01';
+
+        console.log('modified properties', properties);
+
+        blobService.setServiceProperties(properties,function(error) {
+        console.log('Set service callback.');
+
+        if(error) throw error;
+            console.log('done');
+        });
+    });
+
+    /*function setHeaders(blobName){
         var headers = {
             cacheControlHeader:"public, max-age=" + cacheControlLength,
             contentType:"application/octet-stream",
@@ -17,10 +35,7 @@
         blobService.setBlobProperties(containerName, blobName, headers, function(){
             console.log("Header set for: " + blobName);
         });
-
-        blobService.getBlobProperties(containerName, blobName, function (error, blob) {
-            console.log(blob);
-        });
+            
 
     }
 
@@ -39,10 +54,10 @@
 
             for(var index in blobs){
 
-                /*if(isVM(blobs[index].name)){
+                if(isVM(blobs[index].name)){
                     console.log(blobs[index].name);
                     //setHeaders(blobs[index].name);
-                }*/
+                }
                 if(blobs[index].name.indexOf('test/test') > -1){
                     console.log(blobs[index].name);
                     setHeaders(blobs[index].name);
@@ -54,6 +69,6 @@
             console.log(error);
         }
 
-    });
+    });*/
 
 }());
